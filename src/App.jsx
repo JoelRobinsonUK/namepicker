cb3e8c6cb13ef77ac203cd7efcb298b8bb609c30
JoelRobinsonUK <49420265+JoelRobinsonUK@users.noreply.github.com>
2023-11-17 13:16:23 +0000

    ???

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,20 @@ import Add from "./components/Add";
 import Create from "./components/Create";
 import Navbar from "./components/Navbar";
 import Picker from "./components/Piccker";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App h-screen w-screen flex flex-col justify-center">
-        <Navbar />
-        <main className="grow">
-          <Routes>
-            <Route path="/" element={<Picker/>} />
-            <Route path="/create" element={<Create />} />
-          </Routes>
-        </main>
-      </div>
-      <Add/>
-    </BrowserRouter>
+    <div className="App h-screen w-screen flex flex-col justify-center">
+      <Navbar />
+      <main className="grow">
+        <Routes>
+          <Route path="/" element={<Picker />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+      </main>
+      <Add />
+    </div>
+
   );
 }
 
